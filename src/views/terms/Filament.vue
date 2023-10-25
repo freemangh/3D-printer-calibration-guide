bc4a10fb8fbe29d9c4426452a9ff63a8c6716cdf
pavver <pavvers1@gmail.com>
2023-10-26 01:43:30 +0300

    add filament table

--- src/views/terms/Filament.vue
+++ src/views/terms/Filament.vue
@@ -16,73 +16,169 @@
           такі як скло, метал, дерево та інші.
         </p>
         <h5>Основні види філаментів</h5>
+        <q-select
+          label="Про які пластики відображати інформацію"
+          filled
+          multiple
+          clearable
+          v-model="visibleTypes"
+          :options="filamentTypes"
+          :option-value="(opt) => opt.shortName"
+          :option-label="(opt) => opt.shortName + ' (' + opt.name + ')'"
+          emit-value
+          map-options
+        />
         <div
           :class="
             ($windowWidth > 768 ? 'q-ma-md' : 'q-my-md') + ' row justify-center'
           "
         >
-          <div
-            class="col-sm-12 col-md-6 col-lg-4"
-            v-for="filamentType in filamentTypes"
-            :key="filamentType.shortName"
+          <template
+            v-for="filamentInfo in filamentTypesInfo"
+            :key="filamentInfo.type.shortName"
           >
-            <q-card class="q-ma-xs" bordered>
-              <q-toolbar :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-4'">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="28px"
-                  height="28px"
-                  viewBox="0 0 512 512"
+            <div
+              class="col-sm-12 col-md-6 col-lg-4"
+              v-if="
+                visibleTypes.length == 0 ||
+                visibleTypes.includes(filamentInfo.type.shortName)
+              "
+            >
+              <q-card class="q-ma-xs" bordered>
+                <q-toolbar
+                  :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-4'"
                 >
-                  <g :fill="$q.dark.isActive ? 'white' : 'black'">
-                    <path
-                      d="M512,403.9c-10.64,51.13-45.4,72.23-92.76,75-31.74,1.85-63.67.35-96.94.35C321.51,490.63,320.86,500,320,512L269.61,461l52.72-60.5c.67,12.46,1.1,20.45,1.55,28.95,32.44,0,64.46,2.59,95.88-.69,35.59-3.72,49.6-37.7,31.73-71.85-14.17-27.1-29.7-53.47-44.57-80.19-7.19-12.94-14.24-26-22.06-40.26l43.72-25.2c27.21,57.46,68.53,106.2,83.42,168.93Z"
-                    />
-                    <path
-                      d="M266.34,0c38.2,8.94,61.93,35.6,80.57,68.51,11.39,20.12,22.83,40.21,34.9,61.48l24.07-12.42-18.72,76.25-73.45-21.21,25-17.86c-16.57-27.46-31-56.11-49.83-81.27-20.25-27-54-22.27-73,8.32-15.19,24.45-28.82,49.91-43.2,74.89-8,13.9-16.11,27.74-24.73,42.57l-42.37-24.44c4.51-8.35,8.5-15.95,12.68-23.44,15.14-27.06,30.62-53.93,45.45-81.16C181.83,36.87,205.79,10.66,243.07,0Z"
-                    />
-                    <path
-                      d="M0,377.5C9.21,334.66,35.36,300.07,55.09,261l-22.5-14.77,74-21.43c6.12,25.32,11.94,49.39,18.32,75.76L97.09,288.82C83,315.54,66.56,341.05,55.6,368.8,43.05,400.59,65,429.29,99,429.68c40.07.46,80.15.37,120.22.51,4.61,0,9.23,0,13.85,0v48.68c-49.94,0-98.69,2.36-147.11-.7-43.4-2.76-75.48-25.05-86-71.63Z"
-                    />
-                  </g>
-                </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="28px"
+                    height="28px"
+                    viewBox="0 0 512 512"
+                  >
+                    <g :fill="$q.dark.isActive ? 'white' : 'black'">
+                      <path
+                        d="M512,403.9c-10.64,51.13-45.4,72.23-92.76,75-31.74,1.85-63.67.35-96.94.35C321.51,490.63,320.86,500,320,512L269.61,461l52.72-60.5c.67,12.46,1.1,20.45,1.55,28.95,32.44,0,64.46,2.59,95.88-.69,35.59-3.72,49.6-37.7,31.73-71.85-14.17-27.1-29.7-53.47-44.57-80.19-7.19-12.94-14.24-26-22.06-40.26l43.72-25.2c27.21,57.46,68.53,106.2,83.42,168.93Z"
+                      />
+                      <path
+                        d="M266.34,0c38.2,8.94,61.93,35.6,80.57,68.51,11.39,20.12,22.83,40.21,34.9,61.48l24.07-12.42-18.72,76.25-73.45-21.21,25-17.86c-16.57-27.46-31-56.11-49.83-81.27-20.25-27-54-22.27-73,8.32-15.19,24.45-28.82,49.91-43.2,74.89-8,13.9-16.11,27.74-24.73,42.57l-42.37-24.44c4.51-8.35,8.5-15.95,12.68-23.44,15.14-27.06,30.62-53.93,45.45-81.16C181.83,36.87,205.79,10.66,243.07,0Z"
+                      />
+                      <path
+                        d="M0,377.5C9.21,334.66,35.36,300.07,55.09,261l-22.5-14.77,74-21.43c6.12,25.32,11.94,49.39,18.32,75.76L97.09,288.82C83,315.54,66.56,341.05,55.6,368.8,43.05,400.59,65,429.29,99,429.68c40.07.46,80.15.37,120.22.51,4.61,0,9.23,0,13.85,0v48.68c-49.94,0-98.69,2.36-147.11-.7-43.4-2.76-75.48-25.05-86-71.63Z"
+                      />
+                    </g>
+                  </svg>
 
-                <span class="q-pl-lg">
-                  {{ filamentType.name }} ({{ filamentType.shortName }})</span
-                >
-              </q-toolbar>
-              <q-card-section class="q-py-xs">
-                <div class="text-h6 q-mb-xs">
-                  Умови друку {{ filamentType.shortName }}:
-                </div>
-                Температура друку {{ filamentType.conditions.nozzleTemp }}° С<br />
-                Температура стола {{ filamentType.conditions.bedTemp }}° С<br />
-                <span v-for="text in filamentType.conditions.texts" :key="text">
-                  {{ text }} <br />
-                </span>
-              </q-card-section>
-              <q-card-section class="q-py-xs">
-                <div class="text-h6 q-mb-xs">
-                  Умови експлуатації готових деталей:
-                </div>
-                <span v-for="text in filamentType.termsOfUse" :key="text">
-                  {{ text }} <br />
-                </span>
-              </q-card-section>
-              <q-card-section class="q-pb-md q-pt-xs">
-                <div class="text-h6 q-mb-xs">
-                  Умови зберігання {{ filamentType.shortName }} філаменту:
-                </div>
-                <span
-                  v-for="text in filamentType.storageConditions"
-                  :key="text"
-                >
-                  {{ text }} <br />
-                </span>
-              </q-card-section>
-            </q-card>
-          </div>
+                  <span class="q-pl-lg">
+                    {{ filamentInfo.type.name }} ({{
+                      filamentInfo.type.shortName
+                    }})</span
+                  >
+                </q-toolbar>
+                <q-card-section class="q-py-xs">
+                  <div class="text-h6 q-mb-xs">
+                    Умови друку {{ filamentInfo.type.shortName }}:
+                  </div>
+                  Температура друку {{ filamentInfo.conditions.nozzleTemp }}°
+                  С<br />
+                  Температура стола {{ filamentInfo.conditions.bedTemp }}° С<br />
+                  <span
+                    v-for="text in filamentInfo.conditions.texts"
+                    :key="text"
+                  >
+                    {{ text }} <br />
+                  </span>
+                </q-card-section>
+                <q-card-section class="q-py-xs">
+                  <div class="text-h6 q-mb-xs">
+                    Умови експлуатації готових деталей:
+                  </div>
+                  <span v-for="text in filamentInfo.termsOfUse" :key="text">
+                    {{ text }} <br />
+                  </span>
+                </q-card-section>
+                <q-card-section class="q-pb-md q-pt-xs">
+                  <div class="text-h6 q-mb-xs">
+                    Умови зберігання
+                    {{ filamentInfo.type.shortName }} філаменту:
+                  </div>
+                  <span
+                    v-for="text in filamentInfo.storageConditions"
+                    :key="text"
+                  >
+                    {{ text }} <br />
+                  </span>
+                </q-card-section>
+              </q-card>
+            </div>
+          </template>
         </div>
+        <q-table
+          flat
+          bordered
+          title="Виробники"
+          :rows="manufacturers"
+          :columns="manufacturercolumns"
+          row-key="name"
+          hide-bottom
+          hide-pagination
+          v-model:pagination="pagination"
+        >
+          <template v-slot:body-cell-ico="props">
+            <q-td :props="props" style="padding: 0">
+              <q-img
+                :src="props.row.imgUrl"
+                spinner-color="white"
+                style="height: 42px; max-width: 42px; margin: 3px"
+              />
+            </q-td>
+          </template>
+          <template v-slot:body-cell-name="props">
+            <q-td :props="props" style="padding-left: 8px">
+              <span class="text-h5">{{ props.row.name }} </span>
+              <a :href="props.row.siteUrl" target="_blank" class="q-ml-xs">
+                <q-icon name="link" size="32px" />
+              </a>
+            </q-td>
+          </template>
+          <template v-slot:body-cell-types="props">
+            <q-td :props="props">
+              <q-chip
+                v-for="filament in props.row.types"
+                :key="filament.shortName"
+                outline
+                square
+                color="deep-orange"
+                text-color="white"
+              >
+                {{
+                  filament.shortName +
+                  ($windowWidth > 1500 ? " (" + filament.name + ")" : "")
+                }}
+              </q-chip>
+            </q-td>
+          </template>
+          <template v-slot:body-cell-rating="props">
+            <q-td :props="props" style="padding: 0">
+              <a
+                v-if="props.row.rating == null"
+                href="https://docs.google.com/forms/d/e/1FAIpQLSdaqVGhKQrPTRuNOTBs-48-V9OAL3-UyqqAgWJKhi4-n-STmw/viewform"
+                target="_blank"
+              >
+                <q-icon name="link" size="32px" />
+                Тик
+              </a>
+              <q-rating
+                v-else
+                v-model="props.row.rating"
+                readonly
+                max="5"
+                size="42px"
+                color="yellow"
+                icon="star_border"
+                icon-selected="star"
+              />
+            </q-td>
+          </template>
+        </q-table>
         <h3 class="q-mt-none">Вага котушок пластику різних виробників</h3>
         LBL 3kg - 630-650g<br />
         LBL 0.8kg - 220g <br />
@@ -108,12 +204,150 @@ export default defineComponent({
     PopUpImage,
   },
   setup() {
+    const filamentTypes = {
+      pla: { name: "Полілактід", shortName: "PLA" },
+      petg: {
+        name: "Поліетилентерефталат-гліколь",
+        shortName: "PETG",
+      },
+      abs: {
+        name: "Акрилонітрил-бутадієн-стирол",
+        shortName: "ABS",
+      },
+      copet: {
+        name: "Кополіестер",
+        shortName: "CoPET",
+      },
+    };
+    const manufacturers = {
+      plexiwire: {
+        name: "Plexiwire",
+        imgUrl: "https://shop.plexiwire.com.ua/assets/favicon.ico",
+        siteUrl: "https://shop.plexiwire.com.ua/",
+        rating: null,
+        types: [filamentTypes.abs, filamentTypes.pla, filamentTypes.petg],
+      },
+      monofilament: {
+        name: "MonoFilament",
+        imgUrl: "https://monofilament.com.ua/image/catalog/monofav.png",
+        siteUrl: "https://monofilament.com.ua/ua/",
+        rating: null,
+        types: [filamentTypes.abs, filamentTypes.pla, filamentTypes.copet],
+      },
+      tdfilament: {
+        name: "3D filament",
+        imgUrl:
+          "https://images.prom.ua/4633378203_w200_h100_3dfilament-virobnik.jpg",
+        siteUrl: "https://3dfilament.com.ua/ua/",
+        rating: null,
+        types: [filamentTypes.abs, filamentTypes.pla, filamentTypes.petg],
+      },
+      pochatokfilament: {
+        name: "Pochatok filament",
+        imgUrl:
+          "https://pochatok-filament.com.ua/wp-content/themes/filament/assets/img/favicon/apple-touch-icon.png",
+        siteUrl: "https://pochatok-filament.com.ua/",
+        rating: null,
+        types: [filamentTypes.pla],
+      },
+      LBL: {
+        name: "LBL",
+        imgUrl:
+          "https://images.prom.ua/4155800364_w150_h70_lbl-ukrayinskij.jpg",
+        siteUrl: "https://lbl-corp.com/ua/",
+        rating: null,
+        types: [filamentTypes.pla, filamentTypes.copet],
+      },
+      FAINYI: {
+        name: "ФАЙНИЙ",
+        imgUrl: "https://images.prom.ua/4856501795_w200_h100_fainyi.jpg",
+        siteUrl: "https://fainyi3d.com/ua",
+        rating: null,
+        types: [filamentTypes.pla, filamentTypes.petg],
+      },
+      tdplast: {
+        name: "3dplast",
+        imgUrl:
+          "https://images.prom.ua/1405322810_w200_h100_3dplast-virobnik.jpg",
+        siteUrl: "https://3dplast.biz/ua/",
+        rating: null,
+        types: [filamentTypes.abs, filamentTypes.pla, filamentTypes.copet],
+      },
+      U3DF: {
+        name: "U3DF",
+        imgUrl: "https://images.prom.ua/785475509_785475509.ico",
+        siteUrl: "https://u3df.com.ua/ua/",
+        rating: null,
+        types: [filamentTypes.abs, filamentTypes.pla, filamentTypes.petg],
+      },
+      BOZE: {
+        name: "BOZE",
+        imgUrl: "https://www.boze.com.ua/templates/t3_bs3_blank/favicon.ico",
+        siteUrl: "https://www.boze.com.ua/index.php/ru/",
+        rating: null,
+        types: [filamentTypes.abs, filamentTypes.pla],
+      },
+      DASplast: {
+        name: "DASplast",
+        imgUrl: "https://images.prom.ua/620466983_w150_h70_dasplast.jpg",
+        siteUrl: "https://dasplast.com/ua/",
+        rating: null,
+        types: [filamentTypes.abs, filamentTypes.pla],
+      },
+      tDPLASTIC: {
+        name: "3DPLASTIC",
+        imgUrl:
+          "https://www.3dplastic.com.ua/templates/site/images/favicon.ico",
+        siteUrl: "https://www.3dplastic.com.ua/",
+        rating: null,
+        types: [ filamentTypes.petg],
+      },
+      tdbro: {
+        name: "3D BRO",
+        imgUrl:
+          "https://petg.com.ua/wp-content/uploads/2023/02/cropped-favicon-32x32.png",
+        siteUrl: "https://petg.com.ua/",
+        rating: null,
+        types: [ filamentTypes.petg],
+      },
+    };
     return {
       imgUrl: ref(""),
-      filamentTypes: ref([
+      visibleTypes: ref([]),
+      filamentTypes: ref(Object.values(filamentTypes)),
+      manufacturers: ref(Object.values(manufacturers)),
+      manufacturercolumns: ref([
         {
-          name: "Полілактід",
-          shortName: "PLA",
+          name: "ico",
+          label: "",
+        },
+        {
+          name: "name",
+          required: true,
+          label: "Назва",
+          align: "left",
+          sortable: true,
+        },
+        {
+          name: "types",
+          required: true,
+          align: "center",
+          label: "Пластики",
+          sortable: true,
+        },
+        {
+          name: "rating",
+          required: true,
+          label: "Рейтинг",
+          sortable: true,
+        },
+      ]),
+      pagination: ref({
+        rowsPerPage: 100,
+      }),
+      filamentTypesInfo: ref([
+        {
+          type: filamentTypes.pla,
           conditions: {
             nozzleTemp: "190-220",
             bedTemp: "50-60",
@@ -133,8 +367,7 @@ export default defineComponent({
           ],
         },
         {
-          name: "Поліетилентерефталат-гліколь",
-          shortName: "PETG",
+          type: filamentTypes.petg,
           conditions: {
             nozzleTemp: "210-250",
             bedTemp: "80-100",
@@ -153,8 +386,7 @@ export default defineComponent({
           ],
         },
         {
-          name: "Акрилонітрил-бутадієн-стирол",
-          shortName: "ABS",
+          type: filamentTypes.abs,
           conditions: {
             nozzleTemp: "220-240",
             bedTemp: "80-110",
@@ -176,8 +408,7 @@ export default defineComponent({
           ],
         },
         {
-          name: "Кополіестер",
-          shortName: "CoPET",
+          type: filamentTypes.copet,
           conditions: {
             nozzleTemp: "130-260",
             bedTemp: "70-90",
